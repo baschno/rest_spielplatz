15c1e48d36abb1ae63da51950689df74abb9b515
Bastian Schnorbus <bastian.schnorbus@sap.com>
2018-02-26 14:27:44 +0100

    sharing cassandra session for performance improvement

--- bottlerest.py
+++ bottlerest.py
@@ -99,4 +99,5 @@ def delete_handler(name):
 app = application = bottle.default_app()
 
 if __name__ == '__main__':
+    cassandra_connect.setup_connection()
     bottle.run(host = '127.0.0.1', port = 8000)
--- cassandra_connect.py
+++ cassandra_connect.py
@@ -9,19 +9,29 @@ log.addHandler(handler)
 from cassandra.cluster import Cluster
 
 KEYSPACE = "mykeyspace"
+local_session = None
+
+
+def setup_connection():
+    global local_session
+    if local_session is None:
+        cluster = Cluster(['127.0.0.1'], port=9042)
+        local_session = cluster.connect()
+        local_session.set_keyspace(KEYSPACE)
+
+    return local_session
+
 
 def initialize():
-    cluster = Cluster(['127.0.0.1'], port=9042)
-    session = cluster.connect()
-
-    session.execute("""
+    ses = setup_connection()
+    ses.execute("""
                 CREATE KEYSPACE IF NOT EXISTS %s
                 WITH replication = { 'class': 'SimpleStrategy', 'replication_factor': '3' }
                 """ % KEYSPACE)
 
-    session.set_keyspace(KEYSPACE)
+    ses.set_keyspace(KEYSPACE)
 
-    session.execute("""
+    ses.execute("""
              CREATE TABLE IF NOT EXISTS mytable (
                  mykey text,
                  name text,
@@ -35,19 +45,15 @@ def gen_db_key(name):
 
 
 def insert_name(name):
-    cluster = Cluster(['127.0.0.1'], port=9042)
-    session = cluster.connect()
-    session.set_keyspace(KEYSPACE)
+    session = setup_connection()
     query = "INSERT INTO mytable (mykey, name) VALUES (?,?)"
     prepared = session.prepare(query)
     session.execute(prepared, ((gen_db_key(name), name)))
 
 
 def list_names():
-    cluster = Cluster(['127.0.0.1'], port=9042)
-    session = cluster.connect()
-    session.set_keyspace(KEYSPACE)
     resultSet = set()
+    session = setup_connection()
     query = "select * from mytable"
     rows = session.execute(query)
     for row in rows:
@@ -55,9 +61,7 @@ def list_names():
     return resultSet
 
 def remove_name(name):
-    cluster = Cluster(['127.0.0.1'], port=9042)
-    session = cluster.connect()
-    session.set_keyspace(KEYSPACE)
+    session = setup_connection()
     query = "delete from mytable where mykey = '%s'" % gen_db_key(name)
     session.execute(query)
 
