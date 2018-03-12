f45449cccb6868a6c7e24760744557e83659f9e3
d040034 <bastian.schnorbus@sap.com>
2018-03-12 17:09:14 +0100

    adding promise catch to handle errors

--- node/routes/index.js
+++ node/routes/index.js
@@ -4,21 +4,26 @@ var cassandra = require('cassandra-driver');
 var config = require('../config');
 
 var client = new cassandra.Client({
-  contactPoints: [ config.cassandra_seednodes ]
+    contactPoints: [config.cassandra_seednodes]
 });
 
 const query = "SELECT * from mykeyspace.mytable";
-client.execute(query).then(result => console.log('Result %s', result.rows[0].name));
+client.execute(query)
+    .then(result => console.log('Result %s', result.rows[0].name))
+    .catch(function(err) {
+        console.log(err);
+    });
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  client.execute(query, function(err, result) {
-    if (err) {
-      res.status(404).send({msg: err});
-    } else {
-      res.json(result.rows)
-    }
-  })
+router.get('/', function (req, res, next) {
+    client.execute(query, function (err, result) {
+        if (err) {
+            res.status(404).send({ msg: err });
+        } else {
+            res.json(result.rows)
+        }
+    })
 });
 
 module.exports = router;
